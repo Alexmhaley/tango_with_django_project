fb0b2ad57eeb5cc9b6e5e44cab6342aa632b5a92
Alexmhaley <alexanderhaley@gmail.com>
2015-01-26 17:47:54 +0000

    chapter 6 after  excercises

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,15 +3,27 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
+    #views = models.IntegerField(max_length=128, unique = True)
+    #views = 0
+    #likes = models.IntegerField(max_length=128, unique = True)
+    #likes = 0
+    
     def __unicode__(self):
         return self.name
+
+   # def __unicode__(self):
+    #    return self.views
+
+    #def __unicode__(self):
+    #    return self.likes
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default = 0)
 
     def __unicode__(self):
         return self.title
