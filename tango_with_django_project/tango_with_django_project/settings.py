1f8fa01afa181cd6cee40dedc3eaea67465571f9
Alexmhaley <alexanderhaley@gmail.com>
2015-02-28 15:14:39 +0000

    excercise 11 completed

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -100,6 +100,8 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_URL = '/rango/login/'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
